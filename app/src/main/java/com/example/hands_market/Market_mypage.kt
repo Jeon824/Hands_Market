070b6bba8675b64c68701eef4452f18a1d5c9db2
free-elephant <dmsql8811@gmail.com>
2020-11-17 17:28:03 +0900

    Create_market_xml

---
+++ app/src/main/java/com/example/hands_market/Market_mypage.kt
@@ -0,0 +1,11 @@
+package com.example.hands_market
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Market_mypage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_market_mypage2)
+    }
+}
