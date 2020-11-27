fa6fdee27080a7f11f2e7e2c1015cd1791a9bc22
free-elephant <dmsql8811@gmail.com>
2020-11-27 13:21:31 +0900

    Add_User

--- app/src/main/java/com/example/hands_market/User.kt
+++ app/src/main/java/com/example/hands_market/User.kt
@@ -2,16 +2,15 @@ package com.example.hands_market
 
 import android.graphics.Bitmap
 
-//class User(
-//    var userName: String,
-//    var storeName: String,
-//    var storeLat: Double?,
-//    var storeLng: Double?,
-//    var storeAddress: String,
-//    var storeImg: Bitmap? = null,
-//    var storeLayout : Bitmap? = null,
-//    var favoriteStore: Array<String>?=null
-//) {
-//
-//
-//}
+class User(
+    var userName: String,
+    var userId: String,
+    var userPw: String,
+    var phoneNum: String,
+    var email: String,
+    var managerBoolean: Boolean? = null,
+) {
+
+
+}
+
